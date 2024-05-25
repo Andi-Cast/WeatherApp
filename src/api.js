fcc1c5813a3dfb226c0f609904d1cf9468dd90b2
andicast <andi.castillom@gmail.com>
2024-05-24 21:20:13 -0400

    completed working weathera app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '15b78ad87cmshcefe9abd3e8972fp10ede3jsnf95aacfabf54',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "fce95664d5ef8f95222dc7f2951a8485";
